7f8ff509059e837449f15fe2adb05ce01482bdb1
nakayama <kento427@outlook.com>
2019-07-11 16:10:44 +0900

    read csv file
    csvファイルを読みこむところを作成

---
+++ auto select design parameters/auto_select_design_parameters.py
@@ -0,0 +1,16 @@
+import csv
+# https://qiita.com/motoki1990/items/0274d8bcf1a97fe4a869
+
+csvname = 'test.csv'
+path = './../csv/' + csvname
+
+csv_file = open(path, "r", encoding="utf_8-sig", errors="", newline="" )
+f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)	#リスト形式
+
+header = next(f)
+print(header)
+for row in f:
+    #rowはList
+    #row[0]で必要な項目を取得することができる
+    print(row)
+
